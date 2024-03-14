bc350bd750b2d1cd9ee15c6789e77552a60900ab
Robson Alves <robson.curitibapr@gmail.com>
2024-03-14 14:55:52 -0300

    feat: ajustes data

--- src/app/data/Course.ts
+++ src/app/data/Course.ts
@@ -6,125 +6,139 @@ export const Courses: ICourse[] = [
     institution: "Desenvolvedor.IO",
     certificateUrl: "https://desenvolvedor.io/curso/baixar-certificado/74c90a14-45bc-4748-a329-ff76b9f73c8b",
     tags:['development', 'microsoft', 'csharp', '.net'],
-    conclusion: new Date(2023,12,4)
+    conclusion: new Date('2023-12-4')
   },
   {
     name: "Projetando Arquiteturas em Três Camadas",
     institution: "Desenvolvedor.IO",
     certificateUrl: "https://desenvolvedor.io/curso/baixar-certificado/348ea229-b2b7-494c-b588-2bd6ed47f697",
     tags:['development', 'microsoft', 'csharp', '.net', 'architecture'],
-    conclusion: new Date(2023,11,23)
+    conclusion: new Date('2023-11-23')
   },
   {
     name: "Dominando o Kubernetes",
     institution: "Desenvolvedor.IO",
     certificateUrl: "https://desenvolvedor.io/curso/baixar-certificado/2339b4a9-588f-4356-93c0-208678c5489b",
     tags:['kubernetes', 'container'],
-    conclusion: new Date(2023,8,8)
+    conclusion: new Date('2023-8-8')
   },
   {
     name: "Fundamentos do C#",
     institution: "Desenvolvedor.IO",
     certificateUrl: "https://desenvolvedor.io/curso/baixar-certificado/668fdb1c-99e0-4e85-adf4-6b542f71f14e",
     tags:['development', 'microsoft', 'csharp', '.net'],
-    conclusion: new Date(2023,16,1)
+    conclusion: new Date('2023-1-16')
   },
   {
     name: "Dominando o Apache Kafka",
     institution: "Desenvolvedor.IO",
     certificateUrl: "https://desenvolvedor.io/curso/baixar-certificado/5813f9a2-8f15-4682-a239-183a679287ec",
     tags:['development', 'messagerie', 'amqp'],
-    conclusion: new Date(2022,8,12)
+    conclusion: new Date('2022-8-12')
   },
   {
     name: "Fundamentos de Arquitetura de Software",
     institution: "Desenvolvedor.IO",
     certificateUrl: "https://desenvolvedor.io/curso/baixar-certificado/633d37f3-f03f-40d4-a3b6-afd558ccf364",
     tags:['development', 'microsoft', 'csharp', '.net', 'architecture'],
-    conclusion: new Date(2022,9,22)
+    conclusion: new Date('2022-9-22')
   },
   {
     name: "Introdução ao DevOps",
     institution: "Desenvolvedor.IO",
     certificateUrl: "https://desenvolvedor.io/curso/baixar-certificado/bf3a9141-f47e-4d5e-8cca-3e2790245c31",
     tags:['devops'],
-    conclusion: new Date(2022,9,19)
+    conclusion: new Date('2022-9-19')
   },
   {
     name: "Desenvolvimento SPA com Angular",
     institution: "Desenvolvedor.IO",
     certificateUrl: "https://desenvolvedor.io/curso/baixar-certificado/bbb08bef-cc6a-4af1-ab89-36fc72fd2f70",
     tags:['development', 'angular', 'typescript', 'javascript'],
-    conclusion: new Date(2022,8,31)
+    conclusion: new Date('2022-8-31')
   },
   {
     name: "Fundamentos de Criptografia e Hashing",
     institution: "Desenvolvedor.IO",
     certificateUrl: "https://desenvolvedor.io/curso/baixar-certificado/3f08e65b-3f99-40d2-b2f7-3c92f04228b0",
     tags:['crypto', 'hash'],
-    conclusion: new Date(2022,8,22)
+    conclusion: new Date('2022-8-22')
   },
   {
     name: "Iniciando com ASP.NET Core",
     institution: "Desenvolvedor.IO",
     certificateUrl: "https://desenvolvedor.io/curso/baixar-certificado/f9a5fc5f-9bcf-4308-bf17-b0bde1e78378",
     tags:['development', 'microsoft', 'csharp', '.net', 'architecture'],
-    conclusion: new Date(2021,11,4)
+    conclusion: new Date('2021-11-4')
   },
   {
     name: "C# COMPLETO Programação Orientada a Objetos + Projetos",
     institution: "Udemy",
     certificateUrl: "https://www.udemy.com/certificate/UC-26OBZ64N",
     tags:['development', 'microsoft', 'csharp', '.net'],
-    conclusion: new Date(2019,5,16)
+    conclusion: new Date('2019-5-16')
   },
   {
     name: "C# - Aplicando Princípios SOLID na prática",
     institution: "Udemy",
     certificateUrl: "https://www.udemy.com/certificate/UC-70fa6e0e-c9fe-464a-bdbe-7881547c9779",
     tags:['development', 'microsoft', 'csharp', '.net'],
-    conclusion: new Date(2022,6,22)
+    conclusion: new Date('2022-6-22')
   },
   {
     name: "O curso completo de Banco de Dados e SQL, sem mistérios!",
     institution: "Udemy",
     certificateUrl: "https://www.udemy.com/certificate/UC-T3Y1X056",
     tags:['sql', 'microsoft', 'database', 'sqlserver', 'postgres'],
-    conclusion: new Date(2019,6,6)
+    conclusion: new Date('2019-6-6')
   },
   {
     name: "Azure Pipelines - CI/CD, Docker e Kubernetes no Azure DevOps",
     institution: "Udemy",
     certificateUrl: "https://www.udemy.com/certificate/UC-ca99013d-a26a-4121-bdde-dc6c44cc7663",
     tags:['devops', 'microsoft', 'kubernetes', 'docker', 'container'],
-    conclusion: new Date(2021,8,19)
+    conclusion: new Date('2021-8-19')
   },
   {
     name: "Event Driven Architecture - The Complete Guide",
     institution: "Udemy",
     certificateUrl: "https://www.udemy.com/certificate/UC-b711b1a6-8955-47a1-8b5c-bed43c73d0dd",
     tags:['architecture', 'development', 'software'],
-    conclusion: new Date(2023,1,10)
+    conclusion: new Date('2023-1-10')
   },
   {
     name: "Modelagem de Dados UML (Análise&Projeto Orientado a Objetos)",
     institution: "Udemy",
     certificateUrl: "https://www.udemy.com/certificate/UC-018f706e-7090-4b43-9b99-896142a53241",
     tags:['architecture', 'development', 'software', 'uml'],
-    conclusion: new Date(2023,2,3)
+    conclusion: new Date('2023-2-3')
   },
   {
     name: "Microsoft Azure Active Directory",
     institution: "Udemy",
     certificateUrl: "https://www.udemy.com/certificate/UC-72cf91c3-4f64-4674-b560-c4d20570733a",
     tags:['microsoft', 'azure', 'entraid'],
-    conclusion: new Date(2022,11,3)
+    conclusion: new Date('2022-11-3')
   },
   {
     name: "Azure Databases para Iniciantes",
     institution: "Udemy",
     certificateUrl: "https://www.udemy.com/certificate/UC-1629d9df-3184-41c5-83a4-943d89bf3910",
     tags:['microsoft', 'azure', 'sqlserver', 'database'],
-    conclusion: new Date(2022,11,1)
+    conclusion: new Date('2022-11-1')
+  },
+  {
+    name: ".NET 5 e EF Core: relacionando entidades",
+    institution: "Alura",
+    certificateUrl: "https://cursos.alura.com.br/certificate/4779bd73-714f-4631-a190-cf77e64b8f72?lang=pt_BR",
+    tags:['microsoft', 'database', 'orm', 'entity framework', '.net'],
+    conclusion: new Date('2021-7-10')
+  },
+  {
+    name: ".NET 5 e Identity: autenticando e autorizando usuários",
+    institution: "Alura",
+    certificateUrl: "https://cursos.alura.com.br/certificate/af681830-f65c-4cea-a290-ec0ebe460365?lang=pt_BR",
+    tags:['microsoft', 'database', 'orm', 'entity framework', '.net'],
+    conclusion: new Date('2021-5-25')
   }
 ]
