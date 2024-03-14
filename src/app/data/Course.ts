578a3031ef2928b76c22affea8a764be2b97f86a
Robson Alves <111312704+robsonalvesdev@users.noreply.github.com>
2024-03-14 12:08:15 -0300

    feat: inclusão cursos desenvolvedor.io

--- src/app/data/Course.ts
+++ src/app/data/Course.ts
@@ -2,38 +2,73 @@ import { ICourse } from "../interfaces/ICourse";
 
 export const Courses: ICourse[] = [
   {
-    name: "Desenvolvimento em Rust",
-    institution: "Alura",
-    certificateUrl: "https://www.alura.com",
-    tags:['development', 'rust'],
-    conclusion: new Date()
-  },
-  {
-    name: "Desenvolvimento em Go",
+    name: "Iniciando com ASP.NET Core",
     institution: "Desenvolvedor.IO",
-    certificateUrl: "https://www.desenvolvedor.io",
-    tags:['development', 'go'],
-    conclusion: new Date()
+    certificateUrl: "https://desenvolvedor.io/curso/baixar-certificado/74c90a14-45bc-4748-a329-ff76b9f73c8b",
+    tags:['development', 'microsoft', 'csharp', '.net'],
+    conclusion: new Date(2023,12,4)
   },
   {
-    name: "Desenvolvimento em C#",
-    institution: "Udemy",
-    certificateUrl: "https://www.udemy.com.br",
-    tags:['development', 'csharp'],
-    conclusion: new Date()
+    name: "Projetando Arquiteturas em Três Camadas",
+    institution: "Desenvolvedor.IO",
+    certificateUrl: "https://desenvolvedor.io/curso/baixar-certificado/348ea229-b2b7-494c-b588-2bd6ed47f697",
+    tags:['development', 'microsoft', 'csharp', '.net', 'architecture'],
+    conclusion: new Date(2023,11,23)
   },
   {
-    name: "Desenvolvimento em Angular",
-    institution: "Udemy",
-    certificateUrl: "https://www.udemy.com.br",
-    tags:['development', 'angular', 'javascript', 'typescrypt'],
-    conclusion: new Date()
+    name: "Dominando o Kubernetes",
+    institution: "Desenvolvedor.IO",
+    certificateUrl: "https://desenvolvedor.io/curso/baixar-certificado/2339b4a9-588f-4356-93c0-208678c5489b",
+    tags:['kubernetes', 'container'],
+    conclusion: new Date(2023,8,8)
   },
   {
-    name: "Desenvolvimento em Java",
-    institution: "Udemy",
-    certificateUrl: "https://www.udemy.com.br",
-    tags:['development', 'angular', 'java'],
-    conclusion: new Date()
+    name: "Fundamentos do C#",
+    institution: "Desenvolvedor.IO",
+    certificateUrl: "https://desenvolvedor.io/curso/baixar-certificado/668fdb1c-99e0-4e85-adf4-6b542f71f14e",
+    tags:['development', 'microsoft', 'csharp', '.net'],
+    conclusion: new Date(2023,16,1)
+  },
+  {
+    name: "Dominando o Apache Kafka",
+    institution: "Desenvolvedor.IO",
+    certificateUrl: "https://desenvolvedor.io/curso/baixar-certificado/5813f9a2-8f15-4682-a239-183a679287ec",
+    tags:['development', 'messagerie', 'amqp'],
+    conclusion: new Date(2022,8,12)
+  },
+  {
+    name: "Fundamentos de Arquitetura de Software",
+    institution: "Desenvolvedor.IO",
+    certificateUrl: "https://desenvolvedor.io/curso/baixar-certificado/633d37f3-f03f-40d4-a3b6-afd558ccf364",
+    tags:['development', 'microsoft', 'csharp', '.net', 'architecture'],
+    conclusion: new Date(2022,9,22)
+  },
+  {
+    name: "Introdução ao DevOps",
+    institution: "Desenvolvedor.IO",
+    certificateUrl: "https://desenvolvedor.io/curso/baixar-certificado/bf3a9141-f47e-4d5e-8cca-3e2790245c31",
+    tags:['devops'],
+    conclusion: new Date(2022,9,19)
+  },
+  {
+    name: "Desenvolvimento SPA com Angular",
+    institution: "Desenvolvedor.IO",
+    certificateUrl: "https://desenvolvedor.io/curso/baixar-certificado/bbb08bef-cc6a-4af1-ab89-36fc72fd2f70",
+    tags:['development', 'angular', 'typescript', 'javascript'],
+    conclusion: new Date(2022,8,31)
+  },
+  {
+    name: "Fundamentos de Criptografia e Hashing",
+    institution: "Desenvolvedor.IO",
+    certificateUrl: "https://desenvolvedor.io/curso/baixar-certificado/3f08e65b-3f99-40d2-b2f7-3c92f04228b0",
+    tags:['crypto', 'hash'],
+    conclusion: new Date(2022,8,22)
+  },
+  {
+    name: "Iniciando com ASP.NET Core",
+    institution: "Desenvolvedor.IO",
+    certificateUrl: "https://desenvolvedor.io/curso/baixar-certificado/f9a5fc5f-9bcf-4308-bf17-b0bde1e78378",
+    tags:['development', 'microsoft', 'csharp', '.net', 'architecture'],
+    conclusion: new Date(2021,11,4)
   }
 ]
