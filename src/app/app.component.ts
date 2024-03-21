ad6a52662f0b6f63c96753fd6fa4b990e2b6b392
Robson Alves <contato@robsonalves.dev.br>
2024-03-21 16:16:33 -0300

    feat: ajustes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,25 @@
-import { Component } from '@angular/core';
-import { ICourse } from './interfaces/ICourse';
-import { Courses } from './data/Course';
-import { IGraduation } from './interfaces/IGraduation';
-import { Graduations } from './data/Graduation';
-import { IBook } from './interfaces/IBook';
-import { Books } from './data/Book';
-import { IFormationCourse } from './interfaces/IFormationCourse';
-import { FormationCourses } from './data/FormationCourse';
+import { Component } from '@angular/core'
+import { ICourse } from './interfaces/ICourse'
+import { Courses } from './data/Course'
+import { IGraduation } from './interfaces/IGraduation'
+import { Graduations } from './data/Graduation'
+import { IBook } from './interfaces/IBook'
+import { Books } from './data/Book'
+import { IFormationCourse } from './interfaces/IFormationCourse'
+import { FormationCourses } from './data/FormationCourse'
+import { IProfile } from './interfaces/IProfile'
+import { Profile } from './data/Profile'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
-  title = 'Robson Candido dos Santos Alves';
-  courses: ICourse[] = Courses;
-  graduations: IGraduation[] = Graduations;
-  books: IBook[] = Books;
+  title = 'Robson Candido dos Santos Alves'
+  profile: IProfile = Profile
+  courses: ICourse[] = Courses
+  graduations: IGraduation[] = Graduations
+  books: IBook[] = Books
   formationCourses: IFormationCourse[] = FormationCourses
 }
--- src/app/components/pages/contact/contact.component.ts
+++ src/app/components/pages/contact/contact.component.ts
@@ -1,10 +1,12 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input } from '@angular/core'
+import { IProfile } from '../../../interfaces/IProfile'
 
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
-  styleUrls: ['./contact.component.scss']
+  styleUrls: ['./contact.component.scss'],
 })
 export class ContactComponent {
-  @Input({required: false}) bglight: boolean = false;
+  @Input({ required: true }) profiles: IProfile = {} as IProfile
+  @Input({ required: false }) bglight: boolean = false
 }
