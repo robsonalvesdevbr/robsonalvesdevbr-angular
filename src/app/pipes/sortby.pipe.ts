adedf5fb1d8ea2075b9f4a339edcca9efe7a388a
Robson Alves <contato@robsonalves.dev.br>
2024-03-21 20:22:16 -0300

    feat: angular 17

--- src/app/pipes/sortby.pipe.ts
+++ src/app/pipes/sortby.pipe.ts
@@ -1,13 +1,18 @@
-import { Pipe, PipeTransform } from '@angular/core';
-import { orderBy } from 'lodash';
+import { Pipe, PipeTransform } from '@angular/core'
 
 @Pipe({
-  name: 'sortby'
+  name: 'sortby',
 })
 export class SortbyPipe implements PipeTransform {
-
   transform(array: any, campo?: any, order?: any): any[] {
-      return orderBy(array, campo, order);
+    if (array && campo && order) {
+      if (order === 'asc') {
+        return array.sort((a: any, b: any) => (a[campo] > b[campo] ? 1 : -1))
+      } else {
+        return array.sort((a: any, b: any) => (a[campo] < b[campo] ? 1 : -1))
+      }
+    } else {
+      return array.sort((a: any, b: any) => (a > b ? 1 : -1))
+    }
   }
-
 }
