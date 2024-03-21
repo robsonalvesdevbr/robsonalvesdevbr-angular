73d08622e525e5451c6231cabcbdf02f11079ee7
Robson Alves <robson.curitibapr@gmail.com>
2024-03-21 10:37:42 -0300

    feat: prettier

--- src/app/components/pages/course/course.component.ts
+++ src/app/components/pages/course/course.component.ts
@@ -1,12 +1,6 @@
-import {
-  Component,
-  Input,
-  ChangeDetectionStrategy,
-  OnInit,
-} from '@angular/core'
+import { Component, Input, ChangeDetectionStrategy, OnInit } from '@angular/core'
 import { ICourse } from '../../../interfaces/ICourse'
 import { PaginationInstance } from 'ngx-pagination'
-
 @Component({
   selector: 'app-course',
   templateUrl: './course.component.html',
@@ -33,22 +27,15 @@ export class CourseComponent implements OnInit {
   }
 
   ngOnInit(): void {
-    this.courses.forEach((course) =>
-      this.institutions.add(course.institution.trim())
-    )
-    this.courses.forEach((course) =>
-      course.tags.forEach((tag) => this.tags.add(tag.trim()))
-    )
+    this.courses.forEach((course) => this.institutions.add(course.institution.trim()))
+    this.courses.forEach((course) => course.tags.forEach((tag) => this.tags.add(tag.trim())))
   }
 
-  getInstitutions = () =>
-    Array.from(this.institutions.values()).sort((a, b) => (a > b ? 1 : -1))
+  getInstitutions = () => Array.from(this.institutions.values()).sort((a, b) => (a > b ? 1 : -1))
 
-  getTags = () =>
-    Array.from(this.tags.values()).sort((a, b) => (a > b ? 1 : -1))
+  getTags = () => Array.from(this.tags.values()).sort((a, b) => (a > b ? 1 : -1))
 
-  absoluteIndex = (indexOnPage: number): number =>
-    this.config.itemsPerPage * (this.config.currentPage - 1) + indexOnPage + 1
+  absoluteIndex = (indexOnPage: number): number => this.config.itemsPerPage * (this.config.currentPage - 1) + indexOnPage + 1
 
   onPageChange = (number: number) => (this.config.currentPage = number)
 
@@ -56,12 +43,8 @@ export class CourseComponent implements OnInit {
     var link = e.target as HTMLInputElement
     let id = link.id.replace('input_course_institution_', '')
 
-    this.institutionsFilter.has(id)
-      ? this.institutionsFilter.delete(id)
-      : this.institutionsFilter.add(id)
-    this.selectInstitutionsFilter = Array.from(
-      this.institutionsFilter.values()
-    ).join(',')
+    this.institutionsFilter.has(id) ? this.institutionsFilter.delete(id) : this.institutionsFilter.add(id)
+    this.selectInstitutionsFilter = Array.from(this.institutionsFilter.values()).join(',')
 
     this.config.currentPage = 1
 
@@ -83,9 +66,7 @@ export class CourseComponent implements OnInit {
     var link = e.target as HTMLInputElement
     let id = link.id.replace('input_course_tag_', '')
 
-    this.tagsFilter.has(id)
-      ? this.tagsFilter.delete(id)
-      : this.tagsFilter.add(id)
+    this.tagsFilter.has(id) ? this.tagsFilter.delete(id) : this.tagsFilter.add(id)
     this.selectTagFilter = Array.from(this.tagsFilter.values()).join(',')
   }
 }
