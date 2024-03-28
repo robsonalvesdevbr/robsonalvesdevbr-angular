a0785a1d3904f2b95d3f1581784a7b7a533a2d29
Robson Alves <robson.curitibapr@gmail.com>
2024-03-28 09:38:52 -0300

    feat: function

--- src/app/components/base-page/base-page.component.ts
+++ src/app/components/base-page/base-page.component.ts
@@ -1,20 +1,19 @@
-import { Component, Input, OnInit } from '@angular/core';
-import { GoogleAnalyticsService } from 'src/app/services/google-analytics.service';
+import { Component, Input, OnInit } from '@angular/core'
+import { GoogleAnalyticsService } from 'src/app/services/google-analytics.service'
 
 @Component({
-  template: ''
+  template: '',
 })
 export class BasePageComponent implements OnInit {
+  @Input({ required: false }) bglight: boolean = false
 
-  @Input({required: false}) bglight: boolean = false;
-
-  currentClass: Record<string, boolean> = {
-    'bg-light': this.bglight
-  };
-
-  constructor(protected $gaService: GoogleAnalyticsService) { }
-
-  ngOnInit(): void {
+  currentClass = () => {
+    return {
+      'bg-light': this.bglight,
+    }
   }
 
+  constructor(protected $gaService: GoogleAnalyticsService) {}
+
+  ngOnInit(): void {}
 }
