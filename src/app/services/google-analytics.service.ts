a6ccdcb7f48c0d2805ffdb55e955a5916341218e
Robson Alves <111312704+robsonalvesdev@users.noreply.github.com>
2024-04-03 10:13:53 -0300

    feat: eventos de click

--- src/app/services/google-analytics.service.ts
+++ src/app/services/google-analytics.service.ts
@@ -1,46 +1,42 @@
-import { Injectable } from '@angular/core';
-import { filter } from 'rxjs/operators';
-import { environment } from '../../environments/environment';
-import { NavigationEnd, Router } from '@angular/router';
+import { Injectable } from '@angular/core'
+import { filter } from 'rxjs/operators'
+import { environment } from '../../environments/environment'
+import { NavigationEnd, Router } from '@angular/router'
 
-declare var gtag: any;
+declare var gtag: any
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class GoogleAnalyticsService {
-
   constructor(private _router: Router) {
-    this._router.events.pipe(
-      filter(event => event instanceof NavigationEnd)
-    ).subscribe((e) => {
-      if(e instanceof NavigationEnd){
-        gtag('js', new Date());
-        gtag('config', environment.googleAnalytics);
+    this._router.events.pipe(filter((event) => event instanceof NavigationEnd)).subscribe((e) => {
+      if (e instanceof NavigationEnd) {
+        gtag('js', new Date())
+        gtag('config', environment.googleAnalytics)
       }
-    });
+    })
   }
 
   init() {
-    const script = document.createElement('script');
-    script.src = `https://www.googletagmanager.com/gtag/js?id=${environment.googleAnalytics}`;
-    script.async = true;
-    document.getElementsByTagName('head')[0].appendChild(script);
+    const script = document.createElement('script')
+    script.src = `https://www.googletagmanager.com/gtag/js?id=${environment.googleAnalytics}`
+    script.async = true
+    document.getElementsByTagName('head')[0].appendChild(script)
 
-    const gtagEl = document.createElement('script');
+    const gtagEl = document.createElement('script')
     const gtagBody = document.createTextNode(`
       window.dataLayer = window.dataLayer || [];
       function gtag(){dataLayer.push(arguments);}
-    `);
-    gtagEl.appendChild(gtagBody);
-    document.body.appendChild(gtagEl);
+    `)
+    gtagEl.appendChild(gtagBody)
+    document.body.appendChild(gtagEl)
   }
 
-  logEvent(event: string, category: string, label: string, value: any) {
+  logEvent(event: string, category: string, label: string) {
     gtag('event', event, {
       event_category: category,
       event_label: label,
-      value: value
-    });
+    })
   }
 }
