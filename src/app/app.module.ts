2c4a1e3bbe03be45612c967737a0212411db6060
Robson Alves <contato@robsonalves.dev.br>
2024-05-22 21:13:00 -0300

    feat:event repplay

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,16 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core'
+import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser'
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from '@path-app/app.component';
-import { ComponentsModule } from '@path-app/components/components.module';
-import { CommonModule } from '@angular/common';
-import { GoogleAnalyticsService } from '@path-services/google-analytics.service';
+import { AppRoutingModule } from './app-routing.module'
+import { AppComponent } from '@path-app/app.component'
+import { ComponentsModule } from '@path-app/components/components.module'
+import { CommonModule } from '@angular/common'
+import { GoogleAnalyticsService } from '@path-services/google-analytics.service'
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
-    ComponentsModule,
-    CommonModule
-  ],
-  providers: [
-    GoogleAnalyticsService
-  ],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, ComponentsModule, CommonModule],
+  providers: [GoogleAnalyticsService, provideClientHydration(withEventReplay())],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
