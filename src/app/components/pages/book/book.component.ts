3c0c5517516ce749780de9b733ba7c8abb26420a
Robson Alves <contato@robsonalves.dev.br>
2024-05-31 23:48:53 -0300

    feat: signal

--- src/app/components/pages/book/book.component.ts
+++ src/app/components/pages/book/book.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core'
+import { ChangeDetectionStrategy, Component, Input, OnInit, WritableSignal, input, signal } from '@angular/core'
 import { IBook } from '@path-interfaces/IBook'
 import { PaginationInstance } from 'ngx-pagination'
 import { BasePageComponent } from '@path-components/base-page/base-page.component'
@@ -10,71 +10,71 @@ import { BasePageComponent } from '@path-components/base-page/base-page.componen
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class BookComponent extends BasePageComponent implements OnInit {
-  @Input({ required: true }) books: IBook[] = []
+  books = input.required<IBook[]>({ alias: 'books' })
 
-  institutions: Set<string> = new Set<string>()
-  tags: Set<string> = new Set<string>()
+  institutions: WritableSignal<Set<string>> = signal<Set<string>>(new Set<string>())
+  tags: WritableSignal<Set<string>> = signal<Set<string>>(new Set<string>())
 
-  institutionsFilter: Set<string> = new Set<string>()
-  selectInstitutionsFilter: string = ''
+  institutionsFilter: WritableSignal<Set<string>> = signal<Set<string>>(new Set<string>())
+  selectInstitutionsFilter: WritableSignal<string> = signal<string>('')
 
-  tagsFilter: Set<string> = new Set<string>()
-  selectTagFilter: string = ''
+  tagsFilter: WritableSignal<Set<string>> = signal<Set<string>>(new Set<string>())
+  selectTagFilter: WritableSignal<string> = signal<string>('')
 
-  config: PaginationInstance = {
+  config: WritableSignal<PaginationInstance> = signal<PaginationInstance>({
     id: 'booksPag',
     itemsPerPage: 5,
     currentPage: 1,
-  }
+  })
 
   override ngOnInit(): void {
-    this.books.forEach((book) => this.institutions.add(book.publishName.trim()))
-    this.books.forEach((book) => book.tags.forEach((tag) => this.tags.add(tag.trim())))
+    this.books().forEach((book) => this.institutions().add(book.publishName.trim()))
+    this.books().forEach((book) => book.tags.forEach((tag) => this.tags().add(tag.trim())))
     super.ngOnInit()
   }
 
-  getInstitutions = () => Array.from(this.institutions.values()).sort((a, b) => (a > b ? 1 : -1))
+  getInstitutions = () => Array.from(this.institutions().values()).sort((a, b) => (a > b ? 1 : -1))
 
-  getTags = () => Array.from(this.tags.values()).sort((a, b) => (a > b ? 1 : -1))
+  getTags = () => Array.from(this.tags().values()).sort((a, b) => (a > b ? 1 : -1))
 
   absoluteIndex(indexOnPage: number): number {
-    return this.config.itemsPerPage * (this.config.currentPage - 1) + indexOnPage + 1
+    return this.config().itemsPerPage * (this.config().currentPage - 1) + indexOnPage + 1
   }
 
   onPageChange(number: number) {
-    this.config.currentPage = number
+    this.config().currentPage = number
   }
 
   clearFilters() {
-    this.institutionsFilter.forEach((x) => {
+    this.institutionsFilter().forEach((x) => {
       document.getElementById(`label_book_institution_${x}`)?.click()
     })
 
-    this.tagsFilter.forEach((x) => {
+    this.tagsFilter().forEach((x) => {
       document.getElementById(`label_book_tag_${x}`)?.click()
     })
 
-    this.institutionsFilter.clear()
-    this.selectInstitutionsFilter = ''
-    this.tagsFilter.clear()
-    this.selectTagFilter = ''
+    this.institutionsFilter().clear()
+    this.selectInstitutionsFilter.set('')
+    this.tagsFilter().clear()
+    this.selectTagFilter.set('')
   }
 
   onClickIntitutionEvent(e: MouseEvent) {
     var link = e.target as HTMLInputElement
     let id = link.id.replace('input_book_institution_', '')
 
-    this.institutionsFilter.has(id) ? this.institutionsFilter.delete(id) : this.institutionsFilter.add(id)
-    this.selectInstitutionsFilter = Array.from(this.institutionsFilter.values()).join(',')
+    this.institutionsFilter().has(id) ? this.institutionsFilter().delete(id) : this.institutionsFilter().add(id)
+    this.selectInstitutionsFilter.set(Array.from(this.institutionsFilter().values()).join(','))
 
-    this.config.currentPage = 1
+    this.config().currentPage = 1
   }
 
   onClickTagEvent(e: MouseEvent) {
     var link = e.target as HTMLInputElement
     let id = link.id.replace('input_book_tag_', '')
 
-    this.tagsFilter.has(id) ? this.tagsFilter.delete(id) : this.tagsFilter.add(id)
-    this.selectTagFilter = Array.from(this.tagsFilter.values()).join(',')
+    this.tagsFilter().has(id) ? this.tagsFilter().delete(id) : this.tagsFilter().add(id)
+    this.selectTagFilter.set(Array.from(this.tagsFilter().values()).join(','))
   }
 }
--- src/app/components/pages/formationoourse/formationoourse.component.ts
+++ src/app/components/pages/formationoourse/formationoourse.component.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core'
+import { ChangeDetectionStrategy, Component, Input, WritableSignal, input, signal } from '@angular/core'
 import { PaginationInstance } from 'ngx-pagination'
 import { IFormationCourse } from '@path-interfaces/IFormationCourse'
 import { BasePageComponent } from '@path-components/base-page/base-page.component'
@@ -10,19 +10,19 @@ import { BasePageComponent } from '@path-components/base-page/base-page.componen
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class FormationoourseComponent extends BasePageComponent {
-  @Input({ required: true }) formationCourses: IFormationCourse[] = []
+  formationCourses = input.required<IFormationCourse[]>({ alias: 'formationCourses' })
 
-  config: PaginationInstance = {
+  config: WritableSignal<PaginationInstance> = signal<PaginationInstance>({
     id: 'formationCoursesPag',
     itemsPerPage: 5,
     currentPage: 1,
-  }
+  })
 
   absoluteIndex(indexOnPage: number): number {
-    return this.config.itemsPerPage * (this.config.currentPage - 1) + indexOnPage + 1
+    return this.config().itemsPerPage * (this.config().currentPage - 1) + indexOnPage + 1
   }
 
   onPageChange(number: number) {
-    this.config.currentPage = number
+    this.config().currentPage = number
   }
 }
