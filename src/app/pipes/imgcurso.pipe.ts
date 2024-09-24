35eb9529b1421d22eab1a477e2df425f02a7130b
Robson Alves <robson.curitibapr@gmail.com>
2024-09-24 17:10:46 -0300

    refactor: Update filter pipe to support multiple filter values
    The `FilterPipe` in `filter.pipe.ts` has been updated to support multiple filter values separated by commas. This allows for filtering based on multiple criteria at once. The `transform` method now splits the filter string into an array of lowercase values and checks if the field matches any of the filter values. The logic has been improved to handle both string and array fields.
    
    refactor: Update imgcurso pipe to use a map for image lookup
    
    The `ImgcursoPipe` in `imgcurso.pipe.ts` has been updated to use a `Map` for image lookup instead of an object. This change provides better performance and allows for easier addition of new courses and their corresponding images. The `transform` method now uses the `get` method of the `Map` to retrieve the image URL for the given course. A fallback image has also been added in case the course is not found in the map.

--- src/app/pipes/imgcurso.pipe.ts
+++ src/app/pipes/imgcurso.pipe.ts
@@ -1,17 +1,17 @@
-import { Pipe, PipeTransform } from '@angular/core'
+import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
   name: 'imgcurso',
 })
 export class ImgcursoPipe implements PipeTransform {
-  private imagens: { [key: string]: string } = {
-    'Desenvolvedor.IO': 'desenvolvedorio.jpg',
-    Alura: 'alura.jpg',
-    Udemy: 'udemy.png',
-    'Linkedin Learning': 'linkedin.png',
-  }
+  private imagens = new Map<string, string>([
+    ['Desenvolvedor.IO', 'desenvolvedorio.jpg'],
+    ['Alura', 'alura.jpg'],
+    ['Udemy', 'udemy.png'],
+    ['Linkedin Learning', 'linkedin.png'],
+  ]);
 
   transform(curso: 'Alura' | 'Desenvolvedor.IO' | 'Udemy' | 'Linkedin Learning'): string {
-    return this.imagens[curso] || 'default.jpg' // Adicionado fallback
+    return this.imagens.get(curso) || 'default.jpg';
   }
 }
