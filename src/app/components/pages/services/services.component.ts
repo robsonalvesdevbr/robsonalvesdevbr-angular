20b6c852431cad3fc734daeb086162b1cc50bcbb
Robson Alves <robson.curitibapr@gmail.com>
2024-10-14 20:01:04 -0300

    Remove unused team, services, and portfolio grid components

--- src/app/components/pages/services/services.component.ts
+++
@@ -1,9 +0,0 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core'
-import { BasePageComponent } from '@path-components/base-page/base-page.component'
-
-@Component({
-  selector: 'app-services',
-  templateUrl: './services.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush,
-})
-export class ServicesComponent extends BasePageComponent {}
--- src/app/components/pages/team/team.component.ts
+++
@@ -1,9 +0,0 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core'
-import { BasePageComponent } from '@path-components/base-page/base-page.component'
-
-@Component({
-  selector: 'app-team',
-  templateUrl: './team.component.html',
-  changeDetection: ChangeDetectionStrategy.OnPush,
-})
-export class TeamComponent extends BasePageComponent {}
