a744f5b0c7e896a2af403bc42bd4d8a025d522ea
Robson Alves <contato@robsonalves.dev.br>
2024-10-15 09:02:52 -0300

    Refactor app.module.ts to remove GoogleAnalyticsService provider

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,11 @@ import { AppRoutingModule } from './app-routing.module'
 import { AppComponent } from '@path-app/app.component'
 import { ComponentsModule } from '@path-app/components/components.module'
 import { CommonModule } from '@angular/common'
-import { GoogleAnalyticsService } from '@path-services/google-analytics.service'
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, AppRoutingModule, ComponentsModule, CommonModule],
-  providers: [GoogleAnalyticsService, provideExperimentalZonelessChangeDetection()],
+  providers: [provideExperimentalZonelessChangeDetection()],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
