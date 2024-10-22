169f2b4862545767f6d2f6954a7fdaacf3d0e479
Robson Alves <contato@robsonalves.dev.br>
2024-10-21 21:32:03 -0300

    Refactor imports and remove unused code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,49 +1,39 @@
-import { Component, inject, OnInit } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { DataService } from '@path-services/data-service';
-import { GoogleAnalyticsService } from '@path-services/google-analytics.service';
-import { NavigationComponent } from './components/pages/navigation/navigation.component';
-import { MasterheadComponent } from './components/pages/masterhead/masterhead.component';
-import { AboutComponent } from './components/pages/about/about.component';
-import { GraduationComponent } from './components/pages/graduation/graduation.component';
-import { CourseComponent } from './components/pages/course/course.component';
-import { FormationoourseComponent } from './components/pages/formationoourse/formationoourse.component';
-import { BookComponent } from './components/pages/book/book.component';
-import { ContactComponent } from './components/pages/contact/contact.component';
-import { FooterComponent } from './components/pages/footer/footer.component';
+import { Component, inject, OnInit } from '@angular/core'
+import { RouterOutlet } from '@angular/router'
+import { DataService } from '@path-services/data-service'
+import { GoogleAnalyticsService } from '@path-services/google-analytics.service'
+import { NavigationComponent } from './components/pages/navigation/navigation.component'
+import { MasterheadComponent } from './components/pages/masterhead/masterhead.component'
+import { AboutComponent } from './components/pages/about/about.component'
+import { GraduationComponent } from './components/pages/graduation/graduation.component'
+import { CourseComponent } from './components/pages/course/course.component'
+import { FormationoourseComponent } from './components/pages/formationoourse/formationoourse.component'
+import { BookComponent } from './components/pages/book/book.component'
+import { ContactComponent } from './components/pages/contact/contact.component'
+import { FooterComponent } from './components/pages/footer/footer.component'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    RouterOutlet,
-    NavigationComponent,
-    MasterheadComponent,
-    AboutComponent,
-    GraduationComponent,
-    CourseComponent,
-    FormationoourseComponent,
-    BookComponent,
-    ContactComponent,
-    FooterComponent,
-  ],
+  imports: [RouterOutlet, NavigationComponent, MasterheadComponent, AboutComponent, GraduationComponent, CourseComponent, FormationoourseComponent, BookComponent, ContactComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent implements OnInit {
-  title = 'Robson Candido dos Santos Alves';
-  courses = this.dataService.getCourses();
-  graduations = this.dataService.getGraduations();
-  books = this.dataService.getBooks();
-  formationCourses = this.dataService.getFormationCourses();
-  profile = this.dataService.getProfile();
-  private readonly _$gaService = inject(GoogleAnalyticsService);
+  //private readonly _$gaService = inject(GoogleAnalyticsService)
+  private readonly dataService = inject(DataService)
+
+  title = 'Robson Candido dos Santos Alves'
+  courses = this.dataService.getCourses()
+  graduations = this.dataService.getGraduations()
+  books = this.dataService.getBooks()
+  formationCourses = this.dataService.getFormationCourses()
+  profile = this.dataService.getProfile()
+
   //private readonly meta = inject(Meta);
 
-  constructor(private readonly dataService: DataService) {}
-
   ngOnInit(): void {
-    this._$gaService.init();
+    //this._$gaService.init()
     // this.meta.addTag({
     //   name: 'title',
     //   content: 'Robson Candido dos Santos Alves',
--- src/app/services/google-analytics.service.ts
+++ src/app/services/google-analytics.service.ts
@@ -1,75 +1,83 @@
-import { Injectable, inject } from '@angular/core';
-import { filter } from 'rxjs/operators';
-import { environment } from '@path-environments/environment';
-import { NavigationEnd, Router } from '@angular/router';
+import { Injectable, inject } from '@angular/core'
+import { filter } from 'rxjs/operators'
+import { environment } from '@path-environments/environment'
+import { NavigationEnd, Router, Event } from '@angular/router'
+import { Title } from '@angular/platform-browser'
 
-declare let gtag: any;
+declare let gtag: any
 
 @Injectable({
   providedIn: 'root',
 })
 export class GoogleAnalyticsService {
-  private readonly _router = inject(Router);
+  private readonly _router = inject(Router)
+  private readonly _title = inject(Title)
 
   constructor() {
-    this._router.events
-      .pipe(filter((event) => event instanceof NavigationEnd))
-      .subscribe((e) => {
-        if (e instanceof NavigationEnd) {
-          gtag('js', new Date());
-          gtag('config', environment.googleAnalytics, {
-            send_page_view: false,
-            cookie_flags: 'SameSite=None; Secure',
-          });
-        }
-      });
+    this._initRouterEvents()
+    this.init()
   }
 
-  init() {
-    const script = document.createElement('script');
-    script.src = `https://www.googletagmanager.com/gtag/js?id=${environment.googleAnalytics}`;
-    script.async = true;
-    document.getElementsByTagName('head')[0].appendChild(script);
+  private _initRouterEvents(): void {
+    this._router.events.pipe(filter((event: Event): event is NavigationEnd => event instanceof NavigationEnd)).subscribe((event: NavigationEnd) => {
+      this._sendPageView()
+    })
+  }
 
-    const gtagEl = document.createElement('script');
+  private _sendPageView(): void {
+    gtag('js', new Date())
+    gtag('config', environment.googleAnalytics, {
+      send_page_view: false,
+      cookie_flags: 'SameSite=None; Secure',
+    })
+  }
+
+  private init(): void {
+    this._injectAnalyticsScript()
+    this._injectGtagScript()
+  }
+
+  private _injectAnalyticsScript(): void {
+    const script = document.createElement('script')
+    script.src = `https://www.googletagmanager.com/gtag/js?id=${environment.googleAnalytics}`
+    script.async = true
+    document.getElementsByTagName('head')[0].appendChild(script)
+  }
+
+  private _injectGtagScript(): void {
+    const gtagScript = document.createElement('script')
     const gtagBody = document.createTextNode(`
       window.dataLayer = window.dataLayer || [];
       function gtag(){dataLayer.push(arguments);}
-    `);
-    gtagEl.appendChild(gtagBody);
-    document.body.appendChild(gtagEl);
+    `)
+    gtagScript.appendChild(gtagBody)
+    document.body.appendChild(gtagScript)
   }
 
-  logEvent(event: string, category: string, label: string) {
+  logEvent(event: string, category: string, label: string): void {
     gtag('event', event, {
       event_category: category,
       event_label: label,
-    });
+    })
   }
 
-  logSet(
-    campaign: string,
-    id: string,
-    source: string,
-    name: string,
-    term: string
-  ) {
+  logSet(campaign: string, id: string, source: string, name: string, term: string): void {
     gtag('set', campaign, {
       id: id,
       source: source,
       name: name,
       term: term,
-    });
+    })
   }
 
-  logPagView(title: string) {
+  logPageView(title: string): void {
     gtag('event', 'page_view', {
       page_title: title,
-    });
+    })
 
     gtag('event', 'screen_view', {
       app_name: 'robsonalves',
       screen_name: title,
-    });
+    })
   }
 }
