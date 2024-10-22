2dfc94cbd67f98f03f5f743b35831374200f2c2f
Robson Alves <contato@robsonalves.dev.br>
2024-10-21 21:54:39 -0300

    Refactor imports and remove unused code

--- src/app/components/base-page/base-page.component.ts
+++ src/app/components/base-page/base-page.component.ts
@@ -1,12 +1,6 @@
-import { CommonModule } from '@angular/common';
-import {
-  ChangeDetectionStrategy,
-  Component,
-  Input,
-  inject,
-} from '@angular/core';
-import { Router } from '@angular/router';
-import { GoogleAnalyticsService } from '@path-services/google-analytics.service';
+import { CommonModule } from '@angular/common'
+import { ChangeDetectionStrategy, Component, Input, inject } from '@angular/core'
+import { GoogleAnalyticsService } from '@path-services/google-analytics.service'
 
 @Component({
   template: '',
@@ -15,18 +9,12 @@ import { GoogleAnalyticsService } from '@path-services/google-analytics.service'
   standalone: true,
 })
 export class BasePageComponent {
-  @Input({ required: false }) bglight: boolean = false;
-  protected $gaService = inject(GoogleAnalyticsService);
-
-  constructor(private router: Router) {}
+  @Input({ required: false }) bglight: boolean = false
+  protected $gaService = inject(GoogleAnalyticsService)
 
   currentClass = () => {
     return {
       'bg-light': this.bglight,
-    };
-  };
-
-  navigateToSection(sectionId: string): void {
-    this.router.navigate([], { fragment: sectionId });
+    }
   }
 }
