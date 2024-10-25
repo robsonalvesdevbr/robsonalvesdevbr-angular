ab367b3c2d83ad14ecf78763d5bcb9e79e080f68
Robson Alves <robson.curitibapr@gmail.com>
2024-10-24 23:41:14 -0300

    Refactor npm dependencies and Node.js version, add unit tests and karma configuration, and update about component test

---
+++ src/app/components/pages/about/abount.component.spec.ts
@@ -0,0 +1,23 @@
+import { TestBed } from '@angular/core/testing'
+import { AboutComponent } from './about.component'
+
+describe('AbountComponent', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [AboutComponent],
+    }).compileComponents()
+  })
+
+  it('should create the AboutComponent', () => {
+    const fixture = TestBed.createComponent(AboutComponent)
+    const app = fixture.componentInstance
+    expect(app).toBeTruthy()
+  })
+
+  it('should render title', () => {
+    const fixture = TestBed.createComponent(AboutComponent)
+    fixture.detectChanges()
+    const compiled = fixture.nativeElement as HTMLElement
+    expect(compiled.querySelector('div.container div.text-center h3.section-subheading.text-muted')?.textContent).toBe('Saiba mais sobre minha carreira.')
+  })
+})
