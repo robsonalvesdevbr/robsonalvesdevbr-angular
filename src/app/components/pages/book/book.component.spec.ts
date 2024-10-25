c7abc563a774768e5f242755fd783de1cb3b6de3
Robson Alves <contato@robsonalves.dev.br>
2024-10-25 00:10:43 -0300

    Refactor book component unit test

---
+++ src/app/components/pages/book/book.component.spec.ts
@@ -0,0 +1,23 @@
+import { TestBed } from '@angular/core/testing'
+import { BookComponent } from './book.component'
+
+describe('BookComponent', () => {
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [BookComponent],
+    }).compileComponents()
+  })
+
+  it('should create the BookComponent', () => {
+    const fixture = TestBed.createComponent(BookComponent)
+    const app = fixture.componentInstance
+    expect(app).toBeTruthy()
+  })
+
+  xit('should render titles', () => {
+    const fixture = TestBed.createComponent(BookComponent)
+    fixture.detectChanges()
+    const compiled = fixture.nativeElement as HTMLElement
+    expect(compiled.querySelector('div.container div.text-center h2.section-heading.text-uppercase')?.textContent).toBe('Leituras')
+  })
+})
