6bdf696d1d215c2358ca0d75cf4cb9d835aafcfd
Robson Alves <contato@robsonalves.dev.br>
2024-10-24 23:41:14 -0300

    Refactor npm dependencies and Node.js version, add unit tests and karma configuration, and update about component test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,9 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy()
   })
 
-  it(`should have the 'robsonalvesdevbr-angular' title`, () => {
+  it(`should have the 'Robson Candido dos Santos Alves' title`, () => {
     const fixture = TestBed.createComponent(AppComponent)
     const app = fixture.componentInstance
     expect(app.title).toEqual('Robson Candido dos Santos Alves')
-  })
-
-  xit('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent)
-    fixture.detectChanges()
-    const compiled = fixture.nativeElement as HTMLElement
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, robsonalvesdevbr-angular')
   })
 })
