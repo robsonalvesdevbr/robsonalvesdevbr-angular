e35e8a0e6c22675e5c90729d727659015c37c644
Robson Alves <robson.alves@consilux.com.br>
2024-10-29 11:12:38 -0300

    Refactor gitignore to exclude coverage directory

--- karma.conf.js
+++ karma.conf.js
@@ -21,6 +21,14 @@ module.exports = function (config) {
       dir: require('path').join(__dirname, './coverage/robsonalvesdevbr-angular'),
       subdir: '.',
       reporters: [{ type: 'html' }, { type: 'text-summary' }],
+      check: {
+        global: {
+          statements: 80,
+          branches: 80,
+          functions: 80,
+          lines: 80,
+        },
+      },
     },
     reporters: ['progress', 'kjhtml'],
     browsers: ['ChromeHeadless'],
