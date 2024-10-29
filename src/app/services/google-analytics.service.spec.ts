fbe89b10b8d185a1ad8b03619d2248033c043ba9
Robson Alves <robson.curitibapr@gmail.com>
2024-10-29 13:21:19 -0300

    Refactor test scripts to disable watch mode and fetch data from data service

---
+++ src/app/services/google-analytics.service.spec.ts
@@ -0,0 +1,30 @@
+import { TestBed } from '@angular/core/testing'
+import { Router } from '@angular/router'
+import { Title } from '@angular/platform-browser'
+import { GoogleAnalyticsService } from './google-analytics.service'
+
+declare let gtag: any
+
+describe('GoogleAnalyticsService', () => {
+  let service: GoogleAnalyticsService
+  let titleService: Title
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [
+        { provide: Router, useValue: { events: { pipe: () => ({ subscribe: () => {} }) } } },
+        { provide: Title, useValue: { getTitle: () => 'Test Title' } },
+      ],
+    })
+    service = TestBed.inject(GoogleAnalyticsService)
+    titleService = TestBed.inject(Title)
+  })
+
+  it('should be created', () => {
+    expect(service).toBeTruthy()
+  })
+
+  it('should have _title property set', () => {
+    expect(service['_title']).toBeTruthy()
+  })
+})
