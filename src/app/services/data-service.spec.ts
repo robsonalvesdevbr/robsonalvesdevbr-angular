5c11075cab2a328d924d509687cb1c71b2bbd198
Robson Alves <robson.alves@consilux.com.br>
2024-10-29 13:21:19 -0300

    Refactor test scripts to disable watch mode and fetch data from data service

---
+++ src/app/services/data-service.spec.ts
@@ -0,0 +1,45 @@
+import { TestBed } from '@angular/core/testing'
+import { Courses } from '@path-data/Course'
+import { Graduations } from '@path-data/Graduation'
+import { Books } from '@path-data/Book'
+import { FormationCourses } from '@path-data/FormationCourse'
+import { Profile } from '@path-data/Profile'
+import { DataService } from './data-service'
+
+describe('DataService', () => {
+  let service: DataService
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({})
+    service = TestBed.inject(DataService)
+  })
+
+  it('should be created', () => {
+    expect(service).toBeTruthy()
+  })
+
+  it('should return the correct list of courses', () => {
+    const courses = service.getCourses()
+    expect(courses).toEqual(Courses)
+  })
+
+  it('should return the correct list of graduations', () => {
+    const graduations = service.getGraduations()
+    expect(graduations).toEqual(Graduations)
+  })
+
+  it('should return the correct list of books', () => {
+    const books = service.getBooks()
+    expect(books).toEqual(Books)
+  })
+
+  it('should return the correct list of formation courses', () => {
+    const formationCourses = service.getFormationCourses()
+    expect(formationCourses).toEqual(FormationCourses)
+  })
+
+  it('should return the correct profile', () => {
+    const profile = service.getProfile()
+    expect(profile).toEqual(Profile)
+  })
+})
