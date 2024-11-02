b08e60ddc0e75362a81c788ea122d8df2b614101
Robson Alves <robson.curitibapr@gmail.com>
2024-11-01 21:14:13 -0300

    Remove commented-out routing module code from app.routes.ts

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,3 @@
-import { Routes } from '@angular/router';
+import { Routes } from '@angular/router'
 
-export const routes: Routes = [];
-
-// @NgModule({
-//   imports: [
-//     RouterModule.forRoot(routes, {
-//       scrollPositionRestoration: 'enabled',
-//       anchorScrolling: 'enabled',
-//       scrollOffset: [0, 64],
-//     }),
-//   ],
-//   exports: [RouterModule],
-// })
-// export class AppRoutingModule {}
+export const routes: Routes = []
