ccf2a4d3ce026b521a06f5ce6bbf65d6e792ac6c
Robson Alves <robson.curitibapr@gmail.com>
2024-11-04 21:31:45 -0300

    Add workaround for ngClass directive issue in BasePageComponent

--- src/app/components/base-page/base-page.component.ts
+++ src/app/components/base-page/base-page.component.ts
@@ -10,6 +10,7 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core'
 export class BasePageComponent {
   @Input({ required: false }) bglight: boolean = false
 
+  // HACK: This is a workaround for the issue with the `ngClass` directive
   currentClass = () => {
     return {
       'bg-light': this.bglight,
