bbd6dd5279c3c980a69093fd006d66046da80454
Robson Alves <robson.curitibapr@gmail.com>
2024-11-06 21:25:56 -0300

    Refactor code style and formatting across multiple files for consistency

--- src/app/components/pages/footer/footer.component.ts
+++ src/app/components/pages/footer/footer.component.ts
@@ -1,29 +1,17 @@
-import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
-import { GoogleAnalyticsDirective } from '@path-app/directives/google-analytics.directive';
-import { FilterPipe } from '@path-pipes/filter.pipe';
-import { ImgcursoPipe } from '@path-pipes/imgcurso.pipe';
-import { MessageDateConclusionPipe } from '@path-pipes/message-date-conclusion.pipe';
-import { PrintTagsPipe } from '@path-pipes/print-tags.pipe';
-import { SortbyPipe } from '@path-pipes/sortby.pipe';
-import { NgxPaginationModule } from 'ngx-pagination';
+import { CommonModule } from '@angular/common'
+import { ChangeDetectionStrategy, Component } from '@angular/core'
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router'
+import { GoogleAnalyticsDirective } from '@path-app/directives/google-analytics.directive'
+import { FilterPipe } from '@path-pipes/filter.pipe'
+import { ImgcursoPipe } from '@path-pipes/imgcurso.pipe'
+import { MessageDateConclusionPipe } from '@path-pipes/message-date-conclusion.pipe'
+import { PrintTagsPipe } from '@path-pipes/print-tags.pipe'
+import { SortbyPipe } from '@path-pipes/sortby.pipe'
+import { NgxPaginationModule } from 'ngx-pagination'
 
 @Component({
   selector: 'app-footer',
-  imports: [
-    CommonModule,
-    FilterPipe,
-    ImgcursoPipe,
-    MessageDateConclusionPipe,
-    PrintTagsPipe,
-    NgxPaginationModule,
-    GoogleAnalyticsDirective,
-    SortbyPipe,
-    RouterOutlet,
-    RouterLink,
-    RouterLinkActive,
-  ],
+  imports: [CommonModule, FilterPipe, ImgcursoPipe, MessageDateConclusionPipe, PrintTagsPipe, NgxPaginationModule, GoogleAnalyticsDirective, SortbyPipe, RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './footer.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
