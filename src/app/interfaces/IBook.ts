2a9c1f800d7aca6dfdcc4e4671631e45e0bea02a
Robson Alves <contato@robsonalves.dev.br>
2024-11-25 14:21:09 -0300

    Add PublishNameEnum for standardized publisher representation in IBook interface and update BookComponent

--- src/app/interfaces/IBook.ts
+++ src/app/interfaces/IBook.ts
@@ -1,8 +1,10 @@
+import { PublishNameEnum } from "@path-app/models/PublishNameEnum"
+
 export interface IBook {
   title: string
   subtitle: string
   author: string[]
-  publishName: string
+  publishName: PublishNameEnum
   publishYear: number
   tags: string[]
   bookUrl: string | undefined | null
