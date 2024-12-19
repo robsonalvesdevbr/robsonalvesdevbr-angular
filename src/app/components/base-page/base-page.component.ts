1211886e99af06f9e11fe5ec275a2103216f7bf8
Robson Alves <robson.curitibapr@gmail.com>
2024-12-19 14:36:00 -0300

    Refactor BasePageComponent to use input() for bglight property and update currentClass method; enhance HighlightDirective with improved mouse event handling

--- src/app/components/base-page/base-page.component.ts
+++ src/app/components/base-page/base-page.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input } from '@angular/core';
 
 @Component({
   template: '',
@@ -7,12 +7,12 @@ import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class BasePageComponent {
-  @Input({ required: false }) bglight = false;
+  readonly bglight = input(false);
 
   // HACK: This is a workaround for the issue with the `ngClass` directive
   currentClass = () => {
     return {
-      'bg-light': this.bglight,
+      'bg-light': this.bglight(),
     };
   };
 }
