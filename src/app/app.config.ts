c1f92e97b2d4ade0eb14d11d9881c8b6734e2892
Robson Alves <robson.curitibapr@gmail.com>
2025-01-22 14:58:56 -0300

    Add configuration initializer to app.config for startup settings

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,10 +2,12 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideConfigInitializer } from './initializer/startup';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
+    provideConfigInitializer()
   ],
 };
---
+++ src/app/initializer/startup.ts
@@ -0,0 +1,14 @@
+import { provideAppInitializer } from "@angular/core"
+
+// essa função será registrada em app.config.ts
+export function provideConfigInitializer() {
+	// essa função registra um initializer de forma transparente
+	return provideAppInitializer(() => {
+		// faz a injeção do service que permite recuperar as configurações do servidor
+		//const configService = inject(ConfigService)
+
+		// faz a requisição HTTP e mantém as configurações em memória
+		//return configService.getData()
+    console.log('Configurações carregadas no startup...')
+	})
+}
