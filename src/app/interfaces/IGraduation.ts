73c5fd6bbc4c4ef5d6fdc0898fc29a3d23fadebb
Robson Alves <robson.curitibapr@gmail.com>
2025-02-28 09:40:36 -0300

    Add GraduationTagEnum and FormationCourseTagEnum for enhanced categorization of educational tags

--- src/app/interfaces/IGraduation.ts
+++ src/app/interfaces/IGraduation.ts
@@ -1,7 +1,10 @@
+import { GraduationTagEnum } from "@path-app/models/GraduationTagEnum";
+
 export interface IGraduation {
   name: string;
   institution: string;
   conclusionDate: Date | undefined | null;
   conclusion: 'completed' | 'locked' | 'inprogress';
   websiteInstituition: string | undefined | null;
+  tags?: GraduationTagEnum[];
 }
