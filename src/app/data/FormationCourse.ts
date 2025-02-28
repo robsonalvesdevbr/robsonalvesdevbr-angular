2612ebb20b2e21066253ec4ef967e226cf8d2ea0
Robson Alves <contato@robsonalves.dev.br>
2025-02-28 09:40:36 -0300

    Add GraduationTagEnum and FormationCourseTagEnum for enhanced categorization of educational tags

--- src/app/data/FormationCourse.ts
+++ src/app/data/FormationCourse.ts
@@ -1,5 +1,6 @@
 import { InstitutionEnum } from '@path-app/models/InstitutionEnum';
 import { IFormationCourse } from '@path-interfaces/IFormationCourse';
+import { FormationCourseTagEnum } from '../models/FormationCourseTagEnum';
 
 export const FormationCourses: IFormationCourse[] = [
   {
@@ -8,10 +9,10 @@ export const FormationCourses: IFormationCourse[] = [
     certificateUrl:
       'https://cursos.alura.com.br/degree/certificate/2e01d0c3-8f5d-4715-a679-3061632a9298?lang=pt_BR',
     tags: [
-      'software-development',
-      'microsoft-technologies',
-      'csharp',
-      'dotnet',
+      FormationCourseTagEnum.SoftwareDevelopment,
+      FormationCourseTagEnum.MicrosoftTechnologies,
+      FormationCourseTagEnum.CSharp,
+      FormationCourseTagEnum.DotNet,
     ],
     conclusion: new Date('2021-4-26'),
     favorite: true,
@@ -21,7 +22,11 @@ export const FormationCourses: IFormationCourse[] = [
     institution: InstitutionEnum.Alura,
     certificateUrl:
       'https://cursos.alura.com.br/degree/certificate/ecc51fe7-41c9-4eed-afad-5fd31aa1232a?lang=pt_BR',
-    tags: ['kubernetes', 'docker-container', 'azure-cloud'],
+    tags: [
+      FormationCourseTagEnum.Kubernetes,
+      FormationCourseTagEnum.DockerContainer,
+      FormationCourseTagEnum.AzureCloud,
+    ],
     conclusion: new Date('2022-5-13'),
     favorite: true,
   },
@@ -31,12 +36,12 @@ export const FormationCourses: IFormationCourse[] = [
     certificateUrl:
       'https://cursos.alura.com.br/degree/certificate/ef085642-633e-4daa-bdc8-017fd53bd4af?lang=pt_BR',
     tags: [
-      'software-development',
-      'microsoft-technologies',
-      'csharp',
-      'dotnet',
-      'software-testing',
-      'tdd',
+      FormationCourseTagEnum.SoftwareDevelopment,
+      FormationCourseTagEnum.MicrosoftTechnologies,
+      FormationCourseTagEnum.CSharp,
+      FormationCourseTagEnum.DotNet,
+      FormationCourseTagEnum.SoftwareTesting,
+      FormationCourseTagEnum.TDD,
     ],
     conclusion: new Date('2022-5-22'),
     favorite: true,
@@ -46,7 +51,7 @@ export const FormationCourses: IFormationCourse[] = [
     institution: InstitutionEnum.Alura,
     certificateUrl:
       'https://cursos.alura.com.br/degree/certificate/d4c360e6-b8c5-419e-8938-c7337603a927?lang=pt_BR',
-    tags: ['agile', 'scrum'],
+    tags: [FormationCourseTagEnum.Agile, FormationCourseTagEnum.Scrum],
     conclusion: new Date('2022-5-24'),
     favorite: true,
   },
@@ -56,10 +61,10 @@ export const FormationCourses: IFormationCourse[] = [
     certificateUrl:
       'https://cursos.alura.com.br/degree/certificate/51f1efcb-45f2-4005-b875-6890a5a43fe2?lang=pt_BR',
     tags: [
-      'software-development',
-      'microsoft-technologies',
-      'csharp',
-      'dotnet',
+      FormationCourseTagEnum.SoftwareDevelopment,
+      FormationCourseTagEnum.MicrosoftTechnologies,
+      FormationCourseTagEnum.CSharp,
+      FormationCourseTagEnum.DotNet,
     ],
     conclusion: new Date('2022-5-25'),
     favorite: true,
@@ -69,7 +74,11 @@ export const FormationCourses: IFormationCourse[] = [
     institution: InstitutionEnum.Alura,
     certificateUrl:
       'https://cursos.alura.com.br/degree/certificate/ddaea616-7329-4661-a48a-a79db8bf8841?lang=pt_BR',
-    tags: ['software-development', 'google-technologies', 'golang'],
+    tags: [
+      FormationCourseTagEnum.SoftwareDevelopment,
+      FormationCourseTagEnum.GoogleTechnologies,
+      FormationCourseTagEnum.Golang,
+    ],
     conclusion: new Date('2022-6-9'),
     favorite: true,
   },
@@ -78,7 +87,7 @@ export const FormationCourses: IFormationCourse[] = [
     institution: InstitutionEnum.Alura,
     certificateUrl:
       'https://cursos.alura.com.br/degree/certificate/c90fc5d1-85bc-4cd9-860f-5b7e1154e8aa?lang=pt_BR',
-    tags: ['database-management'],
+    tags: [FormationCourseTagEnum.DatabaseManagement],
     conclusion: new Date('2022-8-3'),
     favorite: true,
   },
@@ -87,7 +96,7 @@ export const FormationCourses: IFormationCourse[] = [
     institution: InstitutionEnum.Alura,
     certificateUrl:
       'https://cursos.alura.com.br/degree/certificate/463982aa-7353-4e1b-a775-cf1d574c17b7?lang=pt_BR',
-    tags: ['database-management'],
+    tags: [FormationCourseTagEnum.DatabaseManagement],
     conclusion: new Date('2022-8-12'),
     favorite: true,
   },
@@ -96,7 +105,12 @@ export const FormationCourses: IFormationCourse[] = [
     institution: InstitutionEnum.Alura,
     certificateUrl:
       'https://cursos.alura.com.br/degree/certificate/3314396b-0286-4b16-bb62-636901486dde?lang=pt_BR',
-    tags: ['solid', 'csharp', 'design-pattern', 'software-testing'],
+    tags: [
+      FormationCourseTagEnum.SOLID,
+      FormationCourseTagEnum.CSharp,
+      FormationCourseTagEnum.DesignPattern,
+      FormationCourseTagEnum.SoftwareTesting,
+    ],
     conclusion: new Date('2024-10-22'),
     favorite: true,
   },
@@ -105,7 +119,11 @@ export const FormationCourses: IFormationCourse[] = [
     institution: InstitutionEnum.Alura,
     certificateUrl:
       'https://cursos.alura.com.br/degree/certificate/369add63-2c4a-4949-a6fe-498575a1f773?lang=pt_BR',
-    tags: ['software-development', 'apache-technologies', 'kafka'],
+    tags: [
+      FormationCourseTagEnum.SoftwareDevelopment,
+      FormationCourseTagEnum.ApacheTechnologies,
+      FormationCourseTagEnum.Kafka,
+    ],
     conclusion: new Date('2025-2-3'),
     favorite: true,
   },
@@ -115,7 +133,13 @@ export const FormationCourses: IFormationCourse[] = [
     institution: InstitutionEnum.DesenvolvedorIO,
     certificateUrl:
       'https://desenvolvedor.io/certificado/2b295178-3cae-43bd-8925-98f17501fc8d',
-    tags: ['docker-container', 'ci/cd', 'devops', 'git-github', 'azure-cloud'],
+    tags: [
+      FormationCourseTagEnum.DockerContainer,
+      FormationCourseTagEnum.CICD,
+      FormationCourseTagEnum.DevOps,
+      FormationCourseTagEnum.GitGithub,
+      FormationCourseTagEnum.AzureCloud,
+    ],
     conclusion: new Date('2024-6-11'),
     favorite: true,
   },
