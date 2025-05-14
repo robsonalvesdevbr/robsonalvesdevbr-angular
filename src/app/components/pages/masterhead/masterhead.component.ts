95574dbdeee191cefa2eabdb7caf89959b9f1751
Robson Alves <contato@robsonalves.dev.br>
2025-05-14 14:03:09 -0300

    chore: remove unused CommonModule imports from components

--- src/app/components/pages/masterhead/masterhead.component.ts
+++ src/app/components/pages/masterhead/masterhead.component.ts
@@ -1,11 +1,11 @@
-import { CommonModule } from '@angular/common';
+
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { BasePageComponent } from '@path-components/base-page/base-page.component';
 import { NgxPaginationModule } from 'ngx-pagination';
 
 @Component({
   selector: 'app-masterhead',
-  imports: [CommonModule, NgxPaginationModule],
+  imports: [NgxPaginationModule],
   templateUrl: './masterhead.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
