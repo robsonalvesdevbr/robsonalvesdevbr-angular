65d0f509759a086ba88f81a9be1b6a7fc3a62d2e
Robson Alves <robson.curitibapr@gmail.com>
2025-08-26 23:57:47 -0300

    feat: refatorar componentes e serviços, removendo métodos não utilizados e otimizando a estrutura do código

--- src/app/initializer/startup.ts
+++ src/app/initializer/startup.ts
@@ -1,13 +1,7 @@
-import { inject, provideAppInitializer } from '@angular/core';
-import { ConfigService } from '@path-services/config.service';
+import { provideAppInitializer } from '@angular/core';
 
-// This function will be registered in app.config.ts
 export function provideConfigInitializer() {
-  // Registers an app initializer transparently
   return provideAppInitializer(() => {
-    // Injects the service to retrieve server configurations
-    const configService = inject(ConfigService);
-    // Performs HTTP request and keeps configurations in memory
-    return configService.getData();
+    return Promise.resolve();
   });
 }
